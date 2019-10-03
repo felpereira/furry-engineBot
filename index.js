7bc445e9117d1adc861e97a546d5307cf84419cb
cb.felipe <cb.felipe@gmail.com>
2019-10-03 12:36:53 -0300

    Fist commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var TinyDB = require('tinydb');
+const test_db = new TinyDB('./db.json');
+
+IniciarTimer();
+
+function IniciarTimer() {}
+
+function CarregarAlertas() {}
+
+function EnviarMensgemCanal() {}
+
+function Relogio() {}
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    let mensagem = msg.content;
+
+    if (mensagem.startsWith('!')) {
+        let message = mensagem.substring(
+            mensagem.indexOf('"') + 1,
+            mensagem.lastIndexOf('"')
+        );
+
+        mensagem = mensagem.substring(1).split(' ');
+        console.log(mensagem);
+
+        if (mensagem[0].startsWith('remind')) {
+            let data = mensagem[mensagem.length - 1].split(':');
+
+            var time = new Date();
+            time.setHours(data[0]);
+            time.setMinutes(data[1]);
+
+            mensagem = {
+                where: mensagem[1],
+                when: time,
+                message: message,
+                autor: 'blue'
+            };
+
+            test_db.appendItem(mensagem, () => console.log('teta'));
+        }
+
+        if (msg.content === 'ping') {
+            msg.reply('Pong!');
+        }
+    }
+});
+
+client.login('NjI2OTMwMDgwMTA2NTQ1MTUz.XY1cew.27cLoXB27Tbj1QhC-Seq35j4rGQ');
